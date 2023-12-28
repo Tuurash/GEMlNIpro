d1512e2020d6b43b1d3bed6f9ff3ffc4747ecae5
Tuurash <turasmimoo@gmail.com>
2023-12-28 09:24:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import "./styles/globals.scss";
+import "./styles/markdown.scss";
+import "./styles/highlight.scss";
+import { getClientConfig } from "./config/client";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Gemini Chat Pro - Your personal Gemini Pro Chat Bot",
+  description:
+    "Gemini-Next-Web is the best open source and free chat bot powered by Google Gemini Pro and Gemini Pro Vision models. You can use it to chat with texts and images. All the data are stored in local and there is no need to register an account.",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
+    { media: "(prefers-color-scheme: dark)", color: "#151515" },
+  ],
+  appleWebApp: {
+    title: "Gemini Chat Pro",
+    statusBarStyle: "default",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <script src="/serviceWorkerRegister.js" defer></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
